b611e0f3c5731b7331f53a08570a0425cae9670a
KANO <kanoniba@gmail.com>
2017-12-25 13:49:22 +0800

    modules 拆分

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    MainFooterComponent,
+    ControlSidebarComponent
   ],
   imports: [
     BrowserModule
